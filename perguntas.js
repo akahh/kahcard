dcbfa97221bdb65ad07e611b67e38660f4a77ebb
akahh <00001118652526sp@al.educacao.sp.gov.br>
2024-11-26 08:12:57 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é uma variável?',
+    'Um espaço na memória usado para guardar valores.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o maior continente?',
+    'A Àsia.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é um if?',
+    'uma estrutura para tomar decisões.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'What is the plural of child?',
+    'Children.'
+)
